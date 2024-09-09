3b3bcbda8cde1f006391f05cd1a1b71f35b63d64
maxime_chauvel <chauvel.maxime@valeuriad.fr>
2024-09-09 17:08:10 +0200

    [INIT] OTEL

--- runBuildService.sh
+++ runBuildService.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mvn clean package -DskipTests
 java -javaagent:./opentelemetry-javaagent.jar \
-      -Dotel.service.name=service2 \
+      -Dotel.service.name=service1 \
       -Dotel.traces.exporter=otlp \
       -Dotel.javaagent.debug \
-       -jar ./target/Service2-0.0.1-SNAPSHOT.jar
+       -jar ./target/Service1-0.0.1-SNAPSHOT.jar
