05b7ae077f022be98dc6de28ffb291dcdb4a5b49
maxime_chauvel <chauvel.maxime@valeuriad.fr>
2024-09-11 18:26:59 +0200

    [INIT]  OTEL

--- runBuildService.sh
+++ runBuildService.sh
@@ -4,5 +4,5 @@ PATH_TO_APP="/mnt/c/Users/maxime/Documents/Workspace/Repository/Service1/Service
 java -javaagent:$PATH_TO_APP/opentelemetry-javaagent.jar \
       -Dotel.service.name=service1 \
       -Dotel.traces.exporter=otlp \
-      -Dotel.metrics.exporter=console \
+      -Dotel.logs.exporter=otlp \
        -jar $PATH_TO_APP/target/Service1-0.0.1-SNAPSHOT.jar
--- runService.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-java -javaagent:./opentelemetry-javaagent.jar \
-      -Dotel.service.name=service1 \
-      -Dotel.traces.exporter=otlp \
-      -Dotel.metrics.exporter=otlp \
-       -jar ./target/Service1-0.0.1-SNAPSHOT.jar
